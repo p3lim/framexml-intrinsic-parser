4fe3b770fd0b8543b2f10274da53f6abc5794e02
Adrian L Lange <git@p3lim.net>
2026-09-22 16:19:02 +0200

    Provide the scripts I use to run locally

--- shell.nix
+++ shell.nix
@@ -1,9 +1,54 @@
 let
   pkgs = import <nixpkgs> {};
+
+  scrape = pkgs.writeShellScriptBin "scrape" ''
+    run(){
+      # start with a blank slate
+      rm -rf pages/*
+      clear
+
+      # scrape
+      python3 main.py "$1"
+
+      # show changes
+      git status
+    }
+
+    export -f run
+
+    # watch relevant files and loop
+    ls main.py scraper/* templates/* | entr bash -c "run ''${1}"
+  '';
+
+  wiki = pkgs.writeShellScriptBin "wiki" ''
+    run(){
+      # ensure clean state
+      rm -rf pywikibot.lwp apicache throttle.ctrl
+
+      # source bot secrets
+      source .env
+
+      # run upload
+      python3 wiki.py
+
+      # clean up env and state
+      unset $(sed 's/export //g' .env | cut -d= -f1)
+      rm -rf pywikibot.lwp apicache throttle.ctrl
+    }
+
+    export -f run
+
+    # watch relevant files and loop
+    ls wiki.py | entr bash -c "run"
+  '';
 in pkgs.mkShell {
   packages = [
+    pkgs.entr
     pkgs.python312
     pkgs.python312Packages.venvShellHook
+
+    scrape
+    wiki
   ];
 
   venvDir = "./.venv";
